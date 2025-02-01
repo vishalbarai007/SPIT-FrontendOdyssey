521b7d5a69031751a039cddf9259e60ce42a120c
vbytex <vishalbaraiofficial01@gmail.com>
2025-02-01 10:15:48 +0530

    Added Home & introduction from Aceternity.

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { SidebarDemo } from '../components/AceternityComponents/main/SideBar'
+import DashBoard from './DashBoard'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <SidebarDemo/>
+      <DashBoard/>
+    </div>
+  )
+}
---
+++ Frontend/src/pages/Intro.tsx
@@ -0,0 +1,11 @@
+
+import Carousel from '../components/SplideComponents/Carousel'
+
+export default function Intro() {
+  return (
+    <div>
+      <Carousel/>
+      
+    </div>
+  )
+}
